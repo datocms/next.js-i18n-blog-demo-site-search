c42e68bf5b46913c370a7f6cbdf1b9019f654b29
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2022-11-21 20:07:15 +0100

    searchBar resposiveness

--- components/intro.js
+++ components/intro.js
@@ -2,11 +2,11 @@ import Search from "./search";
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex items-center md:justify-between items-center mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <div>
+      <div className="md:w-1/2 lg:w-1/3 w-full mt-10">
         <Search />
       </div>
     </section>
