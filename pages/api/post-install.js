7df26546ce293a0ab535c68bc3178c4fb86afaa7
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2022-11-21 18:08:36 +0100

    Testing vercel adapter name

--- pages/api/post-install.js
+++ pages/api/post-install.js
@@ -14,27 +14,20 @@ async function vercelInitialization(
         Authorization: `Bearer ${vercelApiToken}`,
       },
       method: "post",
-      body: JSON.stringify({
-        type: "encrypted",
-        key: "NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN_SITE_SEARCH",
-        value: siteSearchToken,
-        target: ["development", "production", "preview"],
-      }),
-    }
-  );
-  await fetch(
-    `https://api.vercel.com/v10/projects/${vercelProjectId}/env?teamId=${vercelTeamId}`,
-    {
-      headers: {
-        Authorization: `Bearer ${vercelApiToken}`,
-      },
-      method: "post",
-      body: JSON.stringify({
-        type: "encrypted",
-        key: "NEXT_EXAMPLE_CMS_DATOCMS_BUILD_TRIGGER_ID",
-        value: buildTriggerId,
-        target: ["development", "production", "preview"],
-      }),
+      body: JSON.stringify([
+        {
+          type: "encrypted",
+          key: "NEXT_EXAMPLE_CMS_DATOCMS_BUILD_TRIGGER_ID",
+          value: buildTriggerId,
+          target: ["development", "production", "preview"],
+        },
+        {
+          type: "encrypted",
+          key: "NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN_SITE_SEARCH",
+          value: siteSearchToken,
+          target: ["development", "production", "preview"],
+        },
+      ]),
     }
   );
 }
@@ -102,6 +95,8 @@ export default async (req, res) => {
         buildTriggerId,
         siteSearchToken
       );
+
+      await client.buildTriggers.trigger(buildTriggerId); //to test if we're getting into this IF
     }
 
     if (req.body.integrationInfo.adapter === "netlify") {
@@ -113,8 +108,6 @@ export default async (req, res) => {
         siteSearchToken
       );
     }
-
-    await client.buildTriggers.trigger(buildTriggerId);
 
     return res.status(200).json({ success: siteSearchToken });
   } catch (error) {
