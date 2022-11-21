e28365a1a5f0d27b3cc3645e9b5cb16a954dc2ab
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2022-11-21 20:25:55 +0100

    Removed lable

--- components/search.js
+++ components/search.js
@@ -21,12 +21,6 @@ export default function Search() {
 
   return (
     <form onSubmit={handleSearch}>
-      <label
-        for="default-search"
-        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-      >
-        Search
-      </label>
       <div className="relative">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg
